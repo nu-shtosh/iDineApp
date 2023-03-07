ec0e28f466b89fb47df675345bbd5219d86d89aa
ILIA <dubenskiiilia@gmail.com>
2023-03-07 14:16:33 +0300

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Илья Дубенский on 07.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
